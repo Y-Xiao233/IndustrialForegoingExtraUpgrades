51c2d2d6a464a1bf2c34818b504596b65eb5a28d
Y-Xiao233 <1870377449@qq.com>
2025-02-10 21:59:07 +0800

    v1.0.1

--- src/main/java/net/yxiao233/ifeu/common/compact/kubejs/schemas/FluidExtractorSchema.java
+++ src/main/java/net/yxiao233/ifeu/common/compact/kubejs/schemas/FluidExtractorSchema.java
@@ -3,15 +3,15 @@ package net.yxiao233.ifeu.common.compact.kubejs.schemas;
 import dev.latvian.mods.kubejs.recipe.RecipeKey;
 import dev.latvian.mods.kubejs.recipe.component.*;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.fluids.FluidStack;
 
 public interface FluidExtractorSchema {
-    RecipeKey<ItemStack> INPUT = ItemStackComponent.ITEM_STACK.inputKey("input");
-    RecipeKey<Block> RESULT = BlockComponent.BLOCK.inputKey("result").allowEmpty();
+    RecipeKey<Ingredient> INPUT = IngredientComponent.INGREDIENT.inputKey("input");
+    RecipeKey<BlockState> RESULT = BlockStateComponent.BLOCK_STRING.inputKey("result").allowEmpty();
     RecipeKey<Float> BREAK_CHANCE = NumberComponent.FLOAT.otherKey("breakChance");
     RecipeKey<FluidStack> OUTPUT = FluidStackComponent.FLUID_STACK.outputKey("output");
-    RecipeKey<Boolean> DEFAULT_RECIPE = BooleanComponent.BOOLEAN.otherKey("defaultRecipe").optional(false);
+    RecipeKey<Boolean> DEFAULT_RECIPE = BooleanComponent.BOOLEAN.otherKey("defaultRecipe").optional(false).allowEmpty();
     RecipeSchema SCHEMA = new RecipeSchema(OUTPUT,INPUT,BREAK_CHANCE,RESULT,DEFAULT_RECIPE);
 }
