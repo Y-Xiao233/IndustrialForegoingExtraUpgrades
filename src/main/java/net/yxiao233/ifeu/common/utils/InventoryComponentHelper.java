35d91c9ec309f3d6156f8acac15f37b3d1c11ec3
Y-Xiao233 <1870377449@qq.com>
2025-02-27 00:36:38 +0800

    v1.1.007

---
+++ src/main/java/net/yxiao233/ifeu/common/utils/InventoryComponentHelper.java
@@ -0,0 +1,59 @@
+package net.yxiao233.ifeu.common.utils;
+
+import com.hrznstudio.titanium.block.tile.MachineTile;
+import com.hrznstudio.titanium.component.inventory.SidedInventoryComponent;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class InventoryComponentHelper {
+    public static boolean canInsertAugment(MachineTile<?> tile, ItemStack stack){
+        if(tile == null || stack == null){
+            return false;
+        }
+
+        if(tile.canAcceptAugment(stack)){
+            SidedInventoryComponent<?> augments = tile.getAugmentInventory();
+            for (int i = 0; i < augments.getSlots(); i++) {
+                ItemStack itemStack = augments.getStackInSlot(i);
+                if(itemStack.isEmpty()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean canInsertAugment(MachineTile<?> tile, Item item){
+        return canInsertAugment(tile,item.getDefaultInstance());
+    }
+
+    public static int getCanInsertIndex(MachineTile<?> tile, ItemStack stack){
+        if(canInsertAugment(tile,stack)){
+            SidedInventoryComponent<?> augments = tile.getAugmentInventory();
+            int index = 0;
+            for (int i = 0; i < augments.getSlots(); i++) {
+                ItemStack itemStack = augments.getStackInSlot(i);
+                if(itemStack.isEmpty()){
+                    index = i;
+                }
+            }
+            return index;
+        }
+        return -1;
+    }
+
+    public static int getCanInsertIndex(MachineTile<?> tile, Item item){
+        return getCanInsertIndex(tile,item.getDefaultInstance());
+    }
+
+    public static void insertAugment(MachineTile<?> tile, ItemStack stack){
+        int index = getCanInsertIndex(tile,stack);
+        if(index != -1){
+            SidedInventoryComponent<?> augments = tile.getAugmentInventory();
+            augments.setStackInSlot(index,stack);
+        }
+    }
+    public static void insertAugment(MachineTile<?> tile, Item item){
+        insertAugment(tile,item.getDefaultInstance());
+    }
+}
