cdb86e6455ece209b08f35e9f600930ffbf0e9b9
Y-Xiao233 <1870377449@qq.com>
2025-06-18 13:40:08 +0800

    v3.1.004

--- src/main/java/net/yxiao233/ifeu/api/item/IFEUAugmentTypes.java
+++ src/main/java/net/yxiao233/ifeu/api/item/IFEUAugmentTypes.java
@@ -3,7 +3,8 @@ package net.yxiao233.ifeu.api.item;
 public enum IFEUAugmentTypes {
     THREAD("thread"),
     APPLE("apple"),
-    SILK("silk");
+    SILK("silk"),
+    HEAL("heal");
     private final String id;
     IFEUAugmentTypes(String id){
         this.id = id;
--- src/main/java/net/yxiao233/ifeu/common/provider/ModSerializableProvider.java
+++ src/main/java/net/yxiao233/ifeu/common/provider/ModSerializableProvider.java
@@ -334,6 +334,16 @@ public class ModSerializableProvider{
                 .inputFluid(new FluidStack(ModFluids.LIQUID_SCULK_MATTER.getSourceFluid().get(),1000))
                 .save(recipeOutput);
 
+        IFEURecipeBuilders.shapedRecipe(Items.ENCHANTED_GOLDEN_APPLE.getDefaultInstance())
+                .pattern("ABA")
+                .pattern("BCB")
+                .pattern("ABA")
+                .define('A', ModContents.DRAGON_STAR.get().getDefaultInstance())
+                .define('B', Tags.Items.STORAGE_BLOCKS_GOLD)
+                .define('C', Items.APPLE.getDefaultInstance())
+                .inputFluid(new FluidStack(ModFluids.LIQUID_SCULK_MATTER.getSourceFluid().get(),100))
+                .save(recipeOutput);
+
         //shapeless
             //有多少输入写多少
         IFEURecipeBuilders.shapelessRecipe(ModContents.DRAGON_STAR.get().getDefaultInstance().copyWithCount(9),"dragon_star_from_block")
